03dc00b0c545b9bfcebbccf4ba9ee5ff01b11782
Ishmael Mavundla <v7.vundla@gmail.com>
2025-07-20 14:23:45 +0200

    Updated

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'LibraryProject.relationship_app',
+    'LibraryProject.relationship_app'
 ]
 
 MIDDLEWARE = [
