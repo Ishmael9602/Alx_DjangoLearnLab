29bacea3b02e53fc091f4be3fb4bea33b2cbe857
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-05 23:06:01 +0200

    Fix CustomUser and Book models; add custom permissions

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+# Custom User
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
         if not username:
@@ -25,4 +26,19 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-from .models_book import Book
+# Book model with custom permissions
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
