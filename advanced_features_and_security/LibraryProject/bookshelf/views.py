54582ddcbee3673b1a34077b44855db5ea09dfaf
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-05 23:23:38 +0200

    Implemented Book model, custom permissions, ExampleForm, views, and templates for advanced_features_and_security task

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,18 +1,8 @@
-from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 from .models import Book
 
-@csrf_protect
-def add_book(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        published_date = request.POST.get('published_date')
-        if title and author and published_date:
-            Book.objects.create(title=title, author=author, published_date=published_date)
-        return redirect('book_list')
-    return render(request, 'bookshelf/form_example.html')
-
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
