b7a42d2fbe7014b60c8472ba1fb94abe22df6673
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-05 23:16:38 +0200

    Implement Django security best practices: CSRF, secure settings, safe views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
 from .models import Book
 
-@permission_required('bookshelf.can_view', raise_exception=True)
+@csrf_protect
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        published_date = request.POST.get('published_date')
+        if title and author and published_date:
+            Book.objects.create(title=title, author=author, published_date=published_date)
+        return redirect('book_list')
+    return render(request, 'bookshelf/form_example.html')
+
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
