22fb0448d6a755f50ffa9ddd8a4259763c739b20
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-05 09:09:24 +0200

    Implement user authentication in Django

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -80,3 +80,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_REDIRECT_URL = '/books/'
+LOGOUT_REDIRECT_URL = '/login/'
