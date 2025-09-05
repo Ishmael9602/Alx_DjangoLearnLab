6dbe68faa04254057e1f290a841c3b8e17199935
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-05 23:57:00 +0200

    Add Book API endpoint with DRF, serializers, views, and URLs

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,19 +1,7 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-@api_view(['GET', 'POST'])
-def book_list(request):
-    if request.method == 'GET':
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
