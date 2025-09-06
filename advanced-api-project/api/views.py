d028842b6dd8b0cbfbb3e6529182efaebf1ae1e9
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-06 21:35:45 +0200

    Fix: add DRF permissions and correct URL patterns

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,25 +1,32 @@
 from rest_framework import generics
 from .serializers import BookSerializer
 from bookshelf.models import Book
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
