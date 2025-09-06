3b70160b3699bc7b8b20a2cf4f93eb3d9e634268
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-06 21:20:06 +0200

    Fix: apply DRF permissions to all views

--- api/views.py
+++ api/views.py
@@ -6,10 +6,12 @@ from bookshelf.models import Book
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
