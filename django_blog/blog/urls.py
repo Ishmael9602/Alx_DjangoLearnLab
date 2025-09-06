1b123a1a68a618c59a5b2a93f72c063c2c1d24a6
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-06 23:18:43 +0200

    Fix: add comment URLs including create/edit/delete

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     # Comment URLs
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('posts/<int:pk>/comments/edit/', CommentUpdateView.as_view(), name='comment-update'),
-    path('posts/<int:pk>/comments/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
