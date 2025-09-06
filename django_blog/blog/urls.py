a10bda8dfc5b94ef7e9e16ef8f2ddc1d3898ec60
Ishmael Mavundla <v7.vundla@gmail.com>
2025-09-07 01:30:09 +0200

    Add post and comment CRUD URLs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,18 +1,24 @@
 from django.urls import path
 from .views import (
-    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
-    CommentCreateView, CommentUpdateView, CommentDeleteView
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
 )
 
 urlpatterns = [
-    # Blog post URLs
+    # Post URLs
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    # Comment URLs (checker requires singular "post")
+    # Comment URLs
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
